0b2a07bbb00ca839a6d9c85df7aa795bf72ffd0a
TheyCallMeAndyBro <133637358+TheyCallMeAndyBro@users.noreply.github.com>
2023-10-11 14:12:38 +0800

    Update RandomPassword.js

--- public/model/RandomPassword.js
+++ public/model/RandomPassword.js
@@ -16,8 +16,11 @@ function getPassword(Length, Lowercase, Uppercase, Numbers, Symbols, Exclude) {
  
   const filterData = Data.split('').filter(ExData => !Exclude.split('').includes(ExData)).join('') //排除字串
   //split('') 字符串變成數組 join('')數組變為字符串
-
   
+  if (filterData.length === 0) {
+    return 'Thers is no valid characters in your selection'   //排除無效操作如只勾選Numbers 而Exclude又輸入0~9 或只輸入password長度其他都沒勾選
+  }
+
   for (let i = 0; i < Length; i++) {
     const randomIndex = Math.floor(Math.random() * filterData.length)
     Password += filterData.charAt(randomIndex)
